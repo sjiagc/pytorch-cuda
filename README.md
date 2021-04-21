e6fd80b3dfa115223598146b37aae75a571e6478
Tom Shen <jun.shen@advance.ai>
2021-04-21 14:47:53 +0800

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # pytorch-cuda
 PyTorch example to consume CUDA input data directly.
+
+## Training
+
+Training is done by python code inside folder training. The dataset used is Fashion MNIST. With a successful training, two model files will be created, model.pth and model.pt. The model.pt file is the Torch Script Model which can be loaded in libtorch.
+
+## libtorch inference
+
+An example of libtorch inference is inside folder libtorch-infer. It includes a piece of hard code input data extracted from the the Fashion MNIST test data set indexed with 100. The model.pt file needs to be in the working directory when running.
---
+++ training/infer.py
@@ -0,0 +1,54 @@
+
+import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+from network import NeuralNetwork
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
+
+m = torch.load('model.pth')
+print(m)
+
+model = NeuralNetwork()
+model.load_state_dict(m)
+model.to(device)
+print(model)
+model.eval()
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+x, y = test_data[100][0], test_data[100][1]
+
+with torch.no_grad():
+    pred = model(x.to(device))
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
+
+model_ts = torch.jit.load("model.pt")
+model_ts.to(device)
+with torch.no_grad():
+    pred = model_ts(x.to(device))
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
+
+
